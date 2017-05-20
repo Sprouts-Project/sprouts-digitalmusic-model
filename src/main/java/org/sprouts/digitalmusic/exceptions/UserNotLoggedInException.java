3845b0cf20d1077b0161f5668e2661354d83d79c
jjxp <javiergc01@gmail.com>
2017-05-20 17:04:42 +0200

    Revert "Added two custom exceptions"
    This reverts commit c9947ae524f5a2d00ac8245cace7b3faa70afda1.

--- src/main/java/org/sprouts/digitalmusic/exceptions/UserNotLoggedInException.java
+++
@@ -1,8 +0,0 @@
-package org.sprouts.digitalmusic.exceptions;
-
-public class UserNotLoggedInException extends Exception {
-
-    public UserNotLoggedInException(String message) {
-        super(message);
-    }
-}
